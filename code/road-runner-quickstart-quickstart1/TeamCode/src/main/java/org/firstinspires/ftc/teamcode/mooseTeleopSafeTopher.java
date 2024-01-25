d8a8d2112492d2f92a50c3c9d899c1874f6f9031
MooseCoding <fontanahome2017@gmail.com>
2024-01-24 22:00:27 -0500

    Update mooseTeleopSafeTopher.java

--- code/road-runner-quickstart-quickstart1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mooseTeleopSafeTopher.java
+++ code/road-runner-quickstart-quickstart1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mooseTeleopSafeTopher.java
@@ -9,7 +9,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @TeleOp
